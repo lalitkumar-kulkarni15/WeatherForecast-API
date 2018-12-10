6733e77a04572b5f4921c42389fa1f886dddbc3c
Hackmaster <33905074+lalitkumar-kulkarni15@users.noreply.github.com>
2018-12-10 14:00:48 +0530

    Update JsonParserSvcImpl.java
    java doc

--- src/main/java/com/weatherforecast/api/service/JsonParserSvcImpl.java
+++ src/main/java/com/weatherforecast/api/service/JsonParserSvcImpl.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * <p>This class has a responsibility of parsing the input json string message and convert it to the
+ * JsonNode object which is used for furthur data processing purpose by the service classes
+ * @author  lalitkumar kulkarni
+ * @since   08-12-2018
+ * @version 1.0
+ */
 @Service
 public class JsonParserSvcImpl implements IParser {
 
